ef2f15db291ab068982d78319c07229271dbf1e7
peter <petermyler100@gmail.com>
2024-02-26 12:16:40 +0000

    Revised decoding function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PIL import Image
-DATA_END = "!EOF"
+DATA_END = "!EOF!"  # special characters that signify the end of the message
 
 
 def decodeData(img_path):
@@ -7,19 +7,25 @@ def decodeData(img_path):
     pixel_values = list(img.getdata())  # get flattened list of all rgb values
 
     full_bin = ""
-    # go through each pixel
     for pixel in pixel_values:
-        # get binary string for each colour value
-        # add last bit of each value to the full string
+        # get binary string for each colour value and combine last bit of each value
         full_bin += sum(bin(p)[-1] for p in pixel)
 
     output = ""
-    # go through full binary string in groups of 8
+    # go through full binary string in groups of 8 bits
     for i in range(0, len(full_bin), 8):
-        byte = full_bin[i:i + 8]  # get 8 bits
-        num = int(byte, 2)
-        if 32 <= num <= 126 or num == 10:
+        # get the 8 bits and convert them to decimal
+        ascii_value = int(full_bin[i:i + 8], 2)
+
+        # make sure the ascii value is valid
+        # if it's not -> append "?" instead
+        if (ascii_value == 10) or (32 <= ascii_value <= 126):
             output += chr(num)
+        else:
+            output += "?"
+
+        # if the last characters equal the special ending characters
+        # return the message (without those characters)
         if output[-len(DATA_END):] == DATA_END:
             return output[:-len(DATA_END)]
 
@@ -51,22 +57,28 @@ def encodeData(img_path, message_str):
             img.putpixel(pos, rgb)
 
 
-# writeImageData("img2.png", "Hi Jamie! I love coding, but really I don\'t.")
-# print(readImageData("img2.png"))
+def main():
+    inp = input("Do you want to encode or decode your image (e/d)? ").lower()
 
-inp = input("Do you want to encode or decode your image (e/d)? ").lower()
-if inp == "e":
-    # encode
-    data = input("Enter the data you want to encode: ")
-    path = input("Enter the image name (without extension): ") + ".png"
-    encodeData(path, data)
-    print("Done!")
-elif inp == "d":
-    # decode
-    path = input("Enter the image name (without extension): ") + ".png"
-    result = decodeData(path)
-    print("Result:\n" + result)
-else:
-    # invalid input
-    print("Error! Invalid input.")
+    if inp == "e":  # encode into image
+        data = input("Enter the message you want to encode: ")
+        path = input("Enter the image filename: ")
+        if "." not in path: path += ".png"
+
+        encodeData(path, data)
+        print("Encoding completed!")
+
+    elif inp == "d":  # decode from image
+        path = input("Enter the image filename: ")
+        if "." not in path: path += ".png"
+
+        result = decodeData(path)
+        if result: print("Result:\n" + result)
+
+    else:
+        print("Error! Invalid input.")
+
+
+if __name__ == "__main__":
+    main()
 
